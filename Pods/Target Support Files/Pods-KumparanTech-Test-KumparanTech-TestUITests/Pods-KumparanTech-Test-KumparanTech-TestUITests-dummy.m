6f1273a4899c38b8d345f4787954bcf838e22bee
adedwi1808 <56765011+adedwi1808@users.noreply.github.com>
2022-01-05 16:51:02 +0700

    full

---
+++ Pods/Target Support Files/Pods-KumparanTech-Test-KumparanTech-TestUITests/Pods-KumparanTech-Test-KumparanTech-TestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KumparanTech_Test_KumparanTech_TestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_KumparanTech_Test_KumparanTech_TestUITests
+@end
